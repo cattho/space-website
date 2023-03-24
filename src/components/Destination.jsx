509ec092079e5cb76ebe367a3418adf9fb7f750b
Sebastian Giraldo <jusegiraldo76@gmail.com>
2023-03-24 10:09:59 -0500

    hover fixes

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 const Destination = () => {
   return (
     <div>
-      sdadas
+      01 Pick your destination
+
+      Moon
+      Mars
+      Europa
+      Titan
+
+      Europa
+
+      The smallest of the four Galilean moons orbiting Jupiter, Europa is a
+      winter lover’s dream. With an icy surface, it’s perfect for a bit of
+      ice skating, curling, hockey, or simple relaxation in your snug
+      wintery cabin.
+
+      Avg. distance
+      628 mil. km
+
+      Est. travel time
+      3 years
     </div>
   )
 }
